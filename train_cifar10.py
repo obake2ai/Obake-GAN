5bc2e54fe2c9667d1dc8f558508788a9196b6846
yuma <fableooon@gmail.com>
2019-03-19 17:49:26 +0900

    Update train_cifar10.py

--- train_cifar10.py
+++ train_cifar10.py
@@ -15,7 +15,7 @@ opt = easydict.EasyDict({
     'b2': 0.999,
     'n_cpu': 8,
     'latent_dim': 128,
-    'img_size': 512,
+    'img_size': 32,
     'n_critic': 1,
     'clip_value':0.01,
     'sample_interval': 100,
@@ -32,13 +32,13 @@ opt = easydict.EasyDict({
 _, dataloader = dataset.makeDataloader(opt)
 
 # Initialize generator and discriminator
-#generator = models.NoiseGenerator2Dv6(opt)
-generator = models.MTNoiseGenerator2Dv6_512(opt, seed=0)
+generator = models.NoiseGenerator2Dv6(opt)
+#generator = models.MTNoiseGenerator2Dv6_512(opt, seed=0)
 #generator = models.MTNoiseGenerator2Dv6(opt, seed=40)
 #generator = models.LCGNoiseGenerator2Dv6_(opt, seed=6)
 #generator = models.NoiseResGenerator2Dv1(opt, noise_layers.NoiseBasicBlock2Dv2, [2,2,2,2,1], level=0.1)
-#discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
-discriminator = naiveresnet.MTNoiseResNet512(naiveresnet.MTNoiseBasicBlock, [1,1,1,1,1,1,1], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seed=0)
+discriminator = naiveresnet.NoiseResNet32(naiveresnet.NoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1)
+#discriminator = naiveresnet.MTNoiseResNet512(naiveresnet.MTNoiseBasicBlock, [1,1,1,1,1,1,1], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seed=0)
 #discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[100, 200, 300], sizes=[8, 8, 4])
 #discriminator = naiveresnet.MTNoiseResNet32(naiveresnet.MTNoiseBasicBlock, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[10, 20,90])
 #discriminator = naiveresnet.LCGNoiseResNet32_(naiveresnet.LCGNoiseBasicBlock_, [2,2,2,2], nchannels=3, nfilters=opt.num_filters, nclasses=1, pool=2, level=0.1, seeds=[7, 8, 9], sizes=[8, 8, 4])
